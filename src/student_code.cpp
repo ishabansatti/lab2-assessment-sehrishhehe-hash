8f6fcf41a5c6663d7261b053fb872b40e903a5ea
sehrishhehe-hash <sehrishhehe@gmail.com>
2026-02-18 15:14:28 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -8,17 +8,24 @@ int getIntValue() {
 }
 
 float getFloatValue() {
-    // Declare a variable var_float of type float and set its valu
+    // Declare a variable var_float of type float and set its values
+    float var_float=10.75;
+    return var_int;
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
+    char var_char="A";
+    return var_char;
 }
 
 int convertFloatToInt(float value) {
     // convert value to int.
+    int result=static_cast<int>(value);
+    return result;
     return value;
+    
 }
 
 /*
@@ -28,6 +35,10 @@ Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
 float fahrenheitToCelsius(float fahrenheit) {
-    //return celsius;
 
+
+
+    float celsius=(farhenheit-32)*5/9;
+    return celsisus;
 }
+    //return celsius
